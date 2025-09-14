d6605873b1d5b969e7d19a0a8f643ef5a468aef9
Ben-Acid15 <bene15.br@gmail.com>
2025-09-14 21:21:04 +0200

    5 - LeafNode

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,5 +1,5 @@
 import unittest
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeadNode
 
 
 class TestHTMLNode(unittest.Testcase):
@@ -12,6 +12,16 @@ class TestHTMLNode(unittest.Testcase):
     def test_two_props(self):
         node = HTMLNode("h2", "A subtitle", None, props={"href": "https://picture2", "target": "__blank"})
         print(f"{self.props_to_html()}")
+
+    def test_to_html_para(self):
+        node = HTMLNode("p", "Hello, world!")
+        self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
+    def test_to_html_tagless(self):
+        node = HTMLNode(None, "Just print me")
+        self.assertEqual(node.to_html(), "Just print me")
+    def test_to_html_error(self):
+        node = HTMLNode("p", None)
+        self.assertNotEqual(node.to_html(), "<p></p>")
 
 
 if __name__ == "__main__":
